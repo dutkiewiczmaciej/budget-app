3dd4f207a3a153cfaa346e64d00fb61a57b467ee
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-08-29 22:20:02 +0200

    add newItem to BudgetController

--- app.js
+++ app.js
@@ -2,10 +2,60 @@
 
 var budgetController = (function(){
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+
+    };
    
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new Item based on 'inc' or 'exp' type 
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
-
 
 
 // UI CONTROLLER
@@ -53,10 +103,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     var ctrlAddItem = function() {
 
-        // 1. Get the field input data
-            var input = UICtrl.getInput();
-        // 2. Add item to the budget controller
+        var input;
+        var newItem;
 
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        // 2. Add item to the budget controller
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value)
         // 3. Add the new item to the UI
 
         // 4. Calculate the budget
