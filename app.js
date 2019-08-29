9c0bc2d68dd4f2ec225dac93e514cf6fa39acb45
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-08-29 19:29:22 +0200

    add DOM strings to all controllers

--- app.js
+++ app.js
@@ -11,13 +11,24 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+
+    }
     return {
         getInput: function() {
             return {
-            type: document.querySelector('.add__type').value, // Will be either inc or exp
-            description: document.querySelector('.add__description').value,
-            value: document.querySelector('.add__value').value
+            type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     };
 
@@ -27,6 +38,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
@@ -41,7 +53,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
