444fa695e45b6c2228efe50170bc57c726e141a7
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-08-29 21:25:22 +0200

    add init function

--- app.js
+++ app.js
@@ -38,12 +38,23 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UIController.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UIController.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+    });
+    }
+
+    
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
             var input = UICtrl.getInput();
-            console.log(input);
         // 2. Add item to the budget controller
 
         // 3. Add the new item to the UI
@@ -51,14 +62,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Aplication has started.');
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
