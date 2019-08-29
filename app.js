108361316e873dd48f1d1be6ba62f4c33952dca8
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-08-29 19:19:49 +0200

    add function to getInput

---
+++ app.js
@@ -0,0 +1,52 @@
+// BUDGET CONTROLLER
+
+var budgetController = (function(){
+
+   
+
+})();
+
+
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    return {
+        getInput: function() {
+            return {
+            type: document.querySelector('.add__type').value, // Will be either inc or exp
+            description: document.querySelector('.add__description').value,
+            value: document.querySelector('.add__value').value
+            }
+        }
+    };
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function() {
+
+        // 1. Get the field input data
+            var input = UICtrl.getInput();
+            console.log(input);
+        // 2. Add item to the budget controller
+
+        // 3. Add the new item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
